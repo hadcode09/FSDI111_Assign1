ef4623bf7abc5ad95bc5470484a588e6a5f8f29e
hadcode09 <mark.hadley1@hotmail.com>
2021-04-21 20:25:59 -0700

    111 assign 1

---
+++ routes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*-- coding: utf8 -*-
+"""HTTP route definitions"""
+
+from app import app #From our app package import the app variable
+
+@app.route("/")
+def index():
+    return "Hello, world!"
+
+@app.route("/about")
+def about():
+    me ={
+        "First_name": "M.A.",
+        "Last_name": "Hadley",
+        "Hobbies": "Gaming & Coding",
+        "About Me": " I try to live my life with a purpose. No matter what mood I'm in."
+    }
+    return me
